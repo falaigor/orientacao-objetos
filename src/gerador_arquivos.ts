4051394681269efa3c355fefd8b938ca7073f665
Igor Santos <falaigors@gmail.com>
2022-01-05 23:44:01 -0300

    Revert "curso orientacao a objetos com TS"
    This reverts commit 9e4310d8bd2451b47dc89bc67e0d74a1453d0e51.

--- src/gerador_arquivos.ts
+++
@@ -1,5 +0,0 @@
-import { writeFileSync } from "fs";
-import { join } from "path";
-import { setLocale, name, phone, internet, date } from "faker";
-
-setLocale("pt_BR");
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ContaBancaria } from "./models/ContaBancaria";
-
-const conta1 = new ContaBancaria("12345-6", "1234-5");
-conta1.depositar(50);
-conta1.sacar(20);
-console.log(conta1);
